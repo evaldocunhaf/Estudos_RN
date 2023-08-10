2348e30db5a17fc161140544c92e3896d41a7bb6
evaldocunhaf <evaldocunhaf@gmail.com>
2023-08-10 16:33:18 -0300

    Consegui adicionar um gravador que vai gravar o resultado das detecções na webcam, ele é ativavel se a variavel gravar no começo do código for True

--- Visao_computacional/detector_emoção_webcam/webcam_hog.py
+++ Visao_computacional/detector_emoção_webcam/webcam_hog.py
@@ -3,6 +3,8 @@ import dlib
 import tensorflow as tf
 import numpy as np
 
+gravar = False
+
 with open('arquitetura_emocoes.json', 'r') as arquivo:
     arquitetura = arquivo.read()
 rede = tf.keras.models.model_from_json(arquitetura)
@@ -14,6 +16,12 @@ emocoes = ['Raiva', 'Desgosto', 'Medo', 'Feliz', 'Neutro', 'Triste', 'Surpreso']
 detector_facial = dlib.get_frontal_face_detector()
 
 captura_camera = cv2.VideoCapture(0)
+
+if gravar:
+    ret, video = captura_camera.read()
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    fps = 10
+    saida_video = cv2.VideoWriter('./resultados_emocoes.mp4', fourcc, fps, (video.shape[1], video.shape[0]))
 
 while True:
     ret, frame = captura_camera.read()
@@ -32,6 +40,8 @@ while True:
             resultado = np.argmax(previsao)
             cv2.putText(frame, emocoes[resultado], (l+40,b+25), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,0,0),1,cv2.LINE_AA)
     cv2.imshow('Video', frame)
+    if gravar:
+        saida_video.write(frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 captura_camera.release()
